8f058fd85fc7c7b33a8a10b3d3c4534e0a641f8a
chouxuewen <49704783+chouxuewen@users.noreply.github.com>
2019-06-16 21:39:03 +0800

    Update Spring.java

--- spring/src/main/java/com/baizhi/Spring.java
+++ spring/src/main/java/com/baizhi/Spring.java
@@ -4,5 +4,6 @@ public class Spring {
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println("2222222222");
+        System.ou.println("版本库打印")
     }
 }
