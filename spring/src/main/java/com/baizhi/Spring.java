c858ba2ef02a2e4a8679bb917e4888532a5d8125
chouxuewen <chouxuewen@163.com>
2019-06-16 21:28:16 +0800

    打印一句话

--- spring/src/main/java/com/baizhi/Spring.java
+++ spring/src/main/java/com/baizhi/Spring.java
@@ -3,5 +3,6 @@ package com.baizhi;
 public class Spring {
     public static void main(String[] args) {
         System.out.println("helloworld");
+        System.out.println("2222222222");
     }
 }
