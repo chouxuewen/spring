3ff8e77ceda267c3ff8b0c2d2575f070b6353d90
chouxuewen <chouxuewen@163.com>
2019-06-16 21:40:08 +0800

    修改语法错误

--- spring/src/main/java/com/baizhi/Spring.java
+++ spring/src/main/java/com/baizhi/Spring.java
@@ -4,6 +4,6 @@ public class Spring {
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println("2222222222");
-        System.ou.println("版本库打印")
+        System.out.println("版本库打印");
     }
 }
